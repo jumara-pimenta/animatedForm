d57897b2ae2f5e3a172a2c84ebbccfa973f32a01
Jumara Pimenta <jumara.pimenta@grupoicts.com.br>
2022-10-21 10:02:23 -0400

    feat: formulário animado finalizado

--- animationForm/01/script.js
+++ animationForm/01/script.js
@@ -11,7 +11,6 @@ btnLogin.addEventListener("click", (event) => {
   });
 
   const formError = document.querySelector(".validate-error");
-
   if (formError) {
     formError.addEventListener("animationend", (event) => {
       if (event.animationName === "nono") {
@@ -30,5 +29,35 @@ form.addEventListener("animationstart", (event) => {
 });
 
 form.addEventListener("animationend", (event) => {
-  if (event.animationName === "down") form.style.display = "none";
+  if (event.animationName === "down") {
+    form.style.display = "none";
+    document.querySelector("body").style.overflow = "none";
+  }
 });
+
+// background squares
+
+const ulSquares = document.querySelector("ul.squares");
+
+for (let i = 0; i < 11; i++) {
+  const li = document.createElement("li");
+
+  const random = (min, max) => Math.random() * (max - min) + min;
+
+  const size = Math.floor(random(10, 120));
+  const position = random(1, 99);
+  const delay = random(5, 0.1);
+  const duration = random(24, 12);
+
+  li.style.width = `${size}px`;
+  li.style.height = `${size}px`;
+  li.style.bottom = `-${size}px`;
+
+  li.style.left = `${position}%`;
+
+  li.style.animationDelay = `${delay}s`;
+  li.style.animationDuration = `${duration}s`;
+  li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`;
+
+  ulSquares.appendChild(li);
+}
